07b47dda4c13273391204aa390aed61dd4e9e827
Amina <amina.anjum016@gmail.com>
2025-06-17 10:35:54 +0500

    Update SECRET_KEY to a secure random value

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'you-should-change-this'
+    SECRET_KEY = '247804eb1bdc277b1679bc079f4f3c5f8e5378744eb0ad6d2d07aa19609eca72'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'orders.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
